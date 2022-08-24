b1c06beee28f66918f59d5b03fe6643d105dc5f8
Dhiraj <dhiraj.c@sonata-software.com>
2022-08-24 17:46:26 +0530

    new

--- src/test/java/com/example/demo/SonatatrainingApplicationTests.java
+++ src/test/java/com/example/demo/SonatatrainingApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sonata.test.Calculator;
 
 @SpringBootTest
 class SonatatrainingApplicationTests {
@@ -9,5 +13,21 @@ class SonatatrainingApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void divTest()
+	{
+		Calculator c3 = new Calculator();
+		int x =c3.div(1,1);
+		assertThrows(ArithmeticException.class,()->c3.div(1,0),"it is an array");
+		
+	}
+	/*@Test
+	public void divTest1()
+	{
+		Calculator c3 = new Calculator();
+		int x =c3.div(1,1);
+		assertThrows(ArithmeticException.class,()->c3.div(1,1),"it is an array");
+		
+	}
+*/
 }
