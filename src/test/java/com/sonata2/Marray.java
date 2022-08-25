5575106d959b1c1b68d86626126ebd65c1095ab0
Dhiraj <dhiraj.c@sonata-software.com>
2022-08-25 14:25:37 +0530

    new

---
+++ src/test/java/com/sonata2/Marray.java
@@ -0,0 +1,13 @@
+package com.sonata2;
+
+public class Marray {
+
+	public static void main(String[] args) {
+		int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+		int x = myNumbers[1][2];
+		System.out.println(x); 
+			
+		}
+	}
+
+
---
+++ src/test/java/com/sonata2/typecasting.java
@@ -0,0 +1,16 @@
+package com.sonata2;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		int myint=8;
+		double mydoubl=9.86d;
+		double mydouble=myint;
+		int myin=(int)mydoubl;
+		System.out.println(myin);
+		System.out.println(myint);
+		
+
+	}
+
+}
